d58a779ee57dfd1b7672364789a385be6be8f67f
lstamellos <lstamellos@gmail.com>
2026-08-10 05:28:59 +0300

    Use native TagDiv title contract only

--- shortcodes/td_block_liveblog.php
+++ shortcodes/td_block_liveblog.php
@@ -24,61 +24,50 @@ class td_block_liveblog extends td_block {
 
 		$this->shortcode_atts = shortcode_atts(
 			array(
-				'title'                 => '',
-				'show_author'           => 'yes',
-				'show_avatar'           => 'yes',
-				'show_timestamp'        => 'yes',
-				'time_display'          => 'exact',
-				'meta_layout'           => 'stacked',
-				'meta_position'         => 'top',
-				'meta_order'            => 'time_author',
-				'meta_alignment'        => 'left',
-				'meta_separator'        => '',
-				'timestamp_prefix'      => '',
-				'author_prefix'         => '',
-				'meta_stack_gap'        => '12',
-				'meta_inline_gap'       => '8',
-				'meta_background'       => '',
-				'meta_text_color'       => '',
-				'meta_padding'          => '0',
-				'meta_gap'              => '8',
-				'entry_background'      => '',
-				'entry_text_color'      => '',
-				'entry_border_color'    => '',
-				'entry_border_width'    => '0',
-				'entry_radius'          => '0',
-				'entry_padding'         => '16',
-				'entry_gap'             => '20',
-				'content_background'    => '',
-				'content_text_color'    => '',
-				'content_border_color'  => '',
-				'content_border_width'  => '0',
-				'content_border_style'  => 'solid',
-				'content_radius'        => '0',
-				'content_padding'       => '0',
-				'timeline'              => 'no',
-				'timeline_color'        => '',
-				'timeline_width'        => '2',
-				'timeline_offset'       => '10',
-
-				// Legacy 0.1.5 attributes kept only for migration of saved canary blocks.
-				'author_position'       => '',
-				'author_order'          => '',
-				'author_alignment'      => '',
-				'timestamp_position'    => '',
-				'timestamp_order'       => '',
-				'timestamp_alignment'   => '',
+				'show_author'          => 'yes',
+				'show_avatar'          => 'yes',
+				'show_timestamp'       => 'yes',
+				'time_display'         => 'exact',
+				'meta_layout'          => 'stacked',
+				'meta_position'        => 'top',
+				'meta_order'           => 'time_author',
+				'meta_alignment'       => 'left',
+				'meta_separator'       => '',
+				'timestamp_prefix'     => '',
+				'author_prefix'        => '',
+				'meta_stack_gap'       => '12',
+				'meta_inline_gap'      => '8',
+				'meta_background'      => '',
+				'meta_text_color'      => '',
+				'meta_padding'         => '0',
+				'meta_gap'             => '8',
+				'entry_background'     => '',
+				'entry_text_color'     => '',
+				'entry_border_color'   => '',
+				'entry_border_width'   => '0',
+				'entry_radius'         => '0',
+				'entry_padding'        => '16',
+				'entry_gap'            => '20',
+				'content_background'   => '',
+				'content_text_color'   => '',
+				'content_border_color' => '',
+				'content_border_width' => '0',
+				'content_border_style' => 'solid',
+				'content_radius'       => '0',
+				'content_padding'      => '0',
+				'timeline'             => 'no',
+				'timeline_color'       => '',
+				'timeline_width'       => '2',
+				'timeline_offset'      => '10',
 			),
 			$raw_atts,
 			'td_block_liveblog'
 		);
 
-		$this->migrate_legacy_meta_atts( $raw_atts );
-
 		/*
 		 * A Global Single template may contain this block for every article.
 		 * Keep the Composer preview available while emitting no frontend wrapper,
-		 * title, spacing or slot at all for posts that are not Liveblog posts.
+		 * header, spacing or slot at all for posts that are not Liveblog posts.
 		 */
 		if ( ! Tagdiv_Liveblog_Plugin::is_composer_request() && ! Tagdiv_Liveblog_Plugin::is_liveblog_post() ) {
 			return '';
@@ -92,19 +81,15 @@ class td_block_liveblog extends td_block {
 		$classes = preg_replace( '/\btd-pb-border-top\b/', '', $this->get_block_classes() );
 		$classes = trim( preg_replace( '/\s+/', ' ', (string) $classes ) ) . ' tdlb-block';
 
-		$style        = $this->build_css_variables();
-		$legacy_title = sanitize_text_field( (string) $this->get_shortcode_att( 'title' ) );
-		$block_title  = $this->get_block_title();
-		$post_id      = Tagdiv_Liveblog_Plugin::get_liveblog_post_id();
+		$style       = $this->build_css_variables();
+		$block_title = $this->get_block_title();
+		$post_id     = Tagdiv_Liveblog_Plugin::get_liveblog_post_id();
 
 		$buffy  = '<div class="' . esc_attr( $classes ) . '">';
 		$buffy .= $this->get_block_css();
 
 		if ( '' !== $block_title ) {
 			$buffy .= '<div class="td-block-title-wrap">' . $block_title . '</div>';
-		} elseif ( '' !== $legacy_title ) {
-			// Backwards compatibility for canary blocks saved before native Header settings were wired up.
-			$buffy .= '<div class="tdlb-title">' . esc_html( $legacy_title ) . '</div>';
 		}
 
 		$buffy .= '<div class="tdlb-liveblog" data-tagdiv-liveblog-slot="1" data-liveblog-post-id="' . esc_attr( (string) absint( $post_id ) ) . '"' . ( $style ? ' style="' . esc_attr( $style ) . '"' : '' ) . '>';
@@ -117,43 +102,6 @@ class td_block_liveblog extends td_block {
 		$buffy .= '</div>';
 
 		return $buffy;
-	}
-
-	/**
-	 * Map 0.1.5's independent metadata controls to the simpler group model.
-	 *
-	 * @param array $raw_atts Original shortcode attributes.
-	 * @return void
-	 */
-	private function migrate_legacy_meta_atts( $raw_atts ) {
-		if ( ! array_key_exists( 'meta_position', $raw_atts ) ) {
-			$author_position    = isset( $raw_atts['author_position'] ) ? sanitize_key( (string) $raw_atts['author_position'] ) : '';
-			$timestamp_position = isset( $raw_atts['timestamp_position'] ) ? sanitize_key( (string) $raw_atts['timestamp_position'] ) : '';
-
-			if ( $author_position && $author_position === $timestamp_position ) {
-				$this->shortcode_atts['meta_position'] = $author_position;
-			}
-		}
-
-		if ( ! array_key_exists( 'meta_order', $raw_atts ) ) {
-			$author_order    = isset( $raw_atts['author_order'] ) ? (string) $raw_atts['author_order'] : '';
-			$timestamp_order = isset( $raw_atts['timestamp_order'] ) ? (string) $raw_atts['timestamp_order'] : '';
-
-			if ( '1' === $author_order && '2' === $timestamp_order ) {
-				$this->shortcode_atts['meta_order'] = 'author_time';
-			} elseif ( '2' === $author_order && '1' === $timestamp_order ) {
-				$this->shortcode_atts['meta_order'] = 'time_author';
-			}
-		}
-
-		if ( ! array_key_exists( 'meta_alignment', $raw_atts ) ) {
-			$author_alignment    = isset( $raw_atts['author_alignment'] ) ? sanitize_key( (string) $raw_atts['author_alignment'] ) : '';
-			$timestamp_alignment = isset( $raw_atts['timestamp_alignment'] ) ? sanitize_key( (string) $raw_atts['timestamp_alignment'] ) : '';
-
-			if ( $author_alignment && $author_alignment === $timestamp_alignment ) {
-				$this->shortcode_atts['meta_alignment'] = $author_alignment;
-			}
-		}
 	}
 
 	/**
