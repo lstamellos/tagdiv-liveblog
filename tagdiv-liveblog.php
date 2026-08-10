d5487710ecab6c3661ea27e25e47cf4c305f91de
lstamellos <lstamellos@gmail.com>
2026-08-10 07:14:34 +0300

    Load pagination adapter and bump version

--- tagdiv-liveblog.php
+++ tagdiv-liveblog.php
@@ -3,7 +3,7 @@
  * Plugin Name:       tagDiv Liveblog
  * Plugin URI:        https://github.com/lstamellos/tagdiv-liveblog
  * Description:       Integrates Automattic Liveblog with Newspaper/tagDiv Composer as a native configurable block.
- * Version:           0.1.12
+ * Version:           0.1.13
  * Requires at least: 6.4
  * Requires PHP:      7.4
  * Author:            Loukas Stamellos
@@ -15,11 +15,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'TAGDIV_LIVEBLOG_VERSION', '0.1.12' );
+define( 'TAGDIV_LIVEBLOG_VERSION', '0.1.13' );
 define( 'TAGDIV_LIVEBLOG_FILE', __FILE__ );
 define( 'TAGDIV_LIVEBLOG_PATH', plugin_dir_path( __FILE__ ) );
 define( 'TAGDIV_LIVEBLOG_URL', plugin_dir_url( __FILE__ ) );
 
 require_once TAGDIV_LIVEBLOG_PATH . 'includes/class-tagdiv-liveblog.php';
+require_once TAGDIV_LIVEBLOG_PATH . 'includes/class-tagdiv-liveblog-pagination.php';
 
 Tagdiv_Liveblog_Plugin::init();
+Tagdiv_Liveblog_Pagination::init();
