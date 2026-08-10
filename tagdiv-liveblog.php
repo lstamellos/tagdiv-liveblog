84e6614e6ac8890fb0e9d4a51773ff70b306df97
lstamellos <lstamellos@gmail.com>
2026-08-10 23:33:35 +0300

    Prepare 0.1.16 rc1 and GitHub updater

--- tagdiv-liveblog.php
+++ tagdiv-liveblog.php
@@ -2,8 +2,9 @@
 /**
  * Plugin Name:       tagDiv Liveblog
  * Plugin URI:        https://github.com/lstamellos/tagdiv-liveblog
+ * Update URI:        https://github.com/lstamellos/tagdiv-liveblog
  * Description:       Integrates Automattic Liveblog with Newspaper/tagDiv Composer as a native configurable block.
- * Version:           0.1.15
+ * Version:           0.1.16-rc1
  * Requires at least: 6.4
  * Requires PHP:      7.4
  * Author:            Loukas Stamellos
@@ -15,13 +16,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'TAGDIV_LIVEBLOG_VERSION', '0.1.15' );
+define( 'TAGDIV_LIVEBLOG_VERSION', '0.1.16-rc1' );
 define( 'TAGDIV_LIVEBLOG_FILE', __FILE__ );
 define( 'TAGDIV_LIVEBLOG_PATH', plugin_dir_path( __FILE__ ) );
 define( 'TAGDIV_LIVEBLOG_URL', plugin_dir_url( __FILE__ ) );
 
 require_once TAGDIV_LIVEBLOG_PATH . 'includes/class-tagdiv-liveblog.php';
 require_once TAGDIV_LIVEBLOG_PATH . 'includes/class-tagdiv-liveblog-pagination.php';
+require_once TAGDIV_LIVEBLOG_PATH . 'includes/class-tagdiv-liveblog-updater.php';
 
 Tagdiv_Liveblog_Plugin::init();
 Tagdiv_Liveblog_Pagination::init();
+Tagdiv_Liveblog_Updater::init();
